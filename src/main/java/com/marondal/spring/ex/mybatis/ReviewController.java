a69ab19d44e43092c2f0e6881107a60786b9f17b
megaIt202 <megait202@gmailcom>
2024-02-22 16:52:53 +0900

    [김인규]  - mybatis insert 예제

--- src/main/java/com/marondal/spring/ex/mybatis/ReviewController.java
+++ src/main/java/com/marondal/spring/ex/mybatis/ReviewController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.marondal.spring.ex.mybatis.domain.Review;
 import com.marondal.spring.ex.mybatis.service.ReviewService;
 
+@RequestMapping("/mybatis/review")
 @Controller
 public class ReviewController {
 	
@@ -17,7 +18,7 @@ public class ReviewController {
 	
 	// request parameter 로 전달된 id와 일치하느 리뷰 정보를 response에 담는다.
 	// /mybatis/review?id=3
-	@RequestMapping("/mybatis/review")
+	@RequestMapping("/")
 	@ResponseBody
 	public Review review(@RequestParam("id") int id) {
 //		int id = Integer.parseInt(request.getParameter("id"));
@@ -25,6 +26,25 @@ public class ReviewController {
 		Review review = reviewService.getReview(id);
 		
 		return review;
+	}
+	
+	@RequestMapping("/create")
+	@ResponseBody
+	public String createReview() {
+		// 4, 치즈피자, 김인규, 4.5
+//		int count = reviewService.addReview(4, "치즈피자", "김인규", 4.5, null);
+		
+		// 2, 뿌링클, 김인규, 4.0, 역시 뿌링클은 진리입니다.
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("김인규");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클은 진리 입니다");
+		
+		int count = reviewService.addReviewByObject(review);
+		
+		return "수행 결과 : " + count;
 	}
 
 }
