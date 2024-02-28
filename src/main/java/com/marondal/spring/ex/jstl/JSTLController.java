9baf192a1a9d7e917ec46e9b62b8bdcb7033549f
megaIt202 <megait202@gmailcom>
2024-02-28 18:09:48 +0900

    [김인규]  - jstl core, format 라이브러리 예제

---
+++ src/main/java/com/marondal/spring/ex/jstl/JSTLController.java
@@ -0,0 +1,66 @@
+package com.marondal.spring.ex.jstl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/jstl")
+@Controller
+public class JSTLController {
+	
+	@GetMapping("/ex01")
+	public String ex01() {
+		return "jstl/ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(Model model) {
+		
+		// 과일이름 목록 리스트
+		List<String> fruitList = new ArrayList<>();
+		fruitList.add("사과");
+		fruitList.add("귤");
+		fruitList.add("바나나");
+		
+		model.addAttribute("fruitList", fruitList);
+		
+		List<Map<String, Object>> memberList = new ArrayList<>();
+		
+		// 이름, 나이, 취미 
+		Map<String, Object> memberMap1 = new HashMap<>();
+		memberMap1.put("name", "유재석");
+		memberMap1.put("age", 52);
+		memberMap1.put("hobby", "진행");
+		
+		Map<String, Object> memberMap2 = new HashMap<>();
+		memberMap2.put("name", "조세호");
+		memberMap2.put("age", 43);
+		memberMap2.put("hobby", "쇼핑");
+		
+		memberList.add(memberMap1);
+		memberList.add(memberMap2);
+		
+		model.addAttribute("memberList", memberList);
+		
+		
+		return "jstl/ex02";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(Model model) {
+		
+		Date now = new Date();
+		
+		model.addAttribute("now", now);
+		
+		return "jstl/ex03";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/jstl/ex02.jsp
@@ -0,0 +1,71 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>JSTL core 제어문</title>
+</head>
+<body>
+
+	<h2>JSTL core 제어문</h2>
+	
+	<h3>1. if else if</h3>
+	<%-- 몸무게가 70이하면 치킨, 80이하면 샐러드, 그게아니면 굶어! --%>
+	<c:set var="weight" value="65" />
+	
+	<c:choose>
+		<c:when test="${weight <= 70 }">
+			<h4>치킨</h4>
+		</c:when>
+		<c:when test="${weight <= 80 }">
+			<h4>샐러드</h4>
+		</c:when>
+		<c:otherwise>
+			<h4>굶어</h4>
+		</c:otherwise>
+	</c:choose>
+	
+	<h3>2. 반복문</h3>
+	
+	<%-- for(int i = 0; i < 5; i++) { } --%>
+	<c:forEach var="i" begin="0" end="4" step="1">
+		${i }
+	</c:forEach>
+	
+	<%-- for(String fruit:fruitList) { } --%>
+	<c:forEach var="fruit" items="${fruitList }" varStatus="status" >
+		<h4>${fruit } :: ${status.count } ${status.index } ${status.first } ${status.last } </h4>
+	</c:forEach>
+	
+	<h3>사용자 리스트</h3>
+	
+	<table border="1">
+		<thead>
+			<tr>
+				<th>이름</th>
+				<th>나이</th>
+				<th>취미</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach var="member" items="${memberList }">
+			<tr>
+				<td>${member.name }</td>
+				<td>${member.age }</td>
+				<td>${member.hobby }</td>
+			</tr>
+			</c:forEach>
+			
+		</tbody>
+	
+	</table>
+	
+	
+	
+	
+	
+
+</body>
+</html>
